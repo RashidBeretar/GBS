3f64d125cae57a45280fc11606c7e88e3cbde59c
Rashid <rashidberetar@gmail.com>
2023-04-13 23:20:14 +0300

    add z15

--- C#_Start/S2_Z15/Program.cs
+++ C#_Start/S2_Z15/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+string IsHoliday(int a)
+{
+    if ((a >= 6) && (a <= 7)) return "да";
+    return "нет";
+}
+
+Console.WriteLine("Введите число:");
+Console.WriteLine(IsHoliday(int.Parse(Console.ReadLine())));
