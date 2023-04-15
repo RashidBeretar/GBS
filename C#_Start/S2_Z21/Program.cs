7fa24378b0df70fb117f65f8a6550bc90f5d3d78
Rashid <rashidberetar@gmail.com>
2023-04-15 23:54:01 +0300

    add t19

---
+++ C#_Start/S2_Z21/Program.cs
@@ -0,0 +1,33 @@
+﻿int ax = 4;
+int ay = 4;
+int bx = 2;
+int by = 4;
+
+double GetDist(int ax, int ay, int bx, int by)
+{
+    return Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2));
+}
+
+Console.WriteLine( GetDist(ax, ay, bx, by) );
+
+// double ax = double.Parse(Console.ReadLine());
+// double ay = 4;
+// double bx = 2;
+// double by = 4;
+
+
+
+// double GetDist(int ax, int ay, int bx, int by)
+// {
+//     return Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2));
+// }
+
+// Console.WriteLine( GetDist(ax, ay, bx, by) );
+
+// double[] a = new double[2];
+// double[] b = new double[2];
+
+// double[] GetCoordFromConsole()
+// {
+
+// }
