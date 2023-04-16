86236f6b6f1aec9270f01260e5a2b38c2525fc07
Rashid <rashidberetar@gmail.com>
2023-04-16 13:13:37 +0300

    add t21

--- C#_Start/T19/Program.cs
+++ C#_Start/T19/Program.cs
@@ -1,4 +1,10 @@
-﻿bool IsPalindrome(int number)
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+bool IsPalindrome(int number)
 {
     if(!(10000 <= number && number <= 99999)) return false;
     if(!(number / 10000 == number % 10)) return false;
---
+++ C#_Start/T21/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+void PrintArray(double[] array)
+{
+    string str = "";
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        str += (i < length - 1) ? array[i] + ", " : array[i];
+    }
+    Console.WriteLine(str);
+}
+double[] GetArrayFromString(string arrayAsString)
+{
+    string[] strings = arrayAsString.Split("; ");
+    int length = strings.Length;
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = double.Parse(strings[i]);
+    }
+    return array;
+}
+double GetDist2Point(double[] A, double[] B)
+{
+    if(A.Length != B.Length)
+    {
+        Console.WriteLine("Ошибка! Координаты заданы не верно");
+        return -1;
+    }
+    double forsqrt = 0;
+    for(int i = 0; i < A.Length; i++)
+    {
+      forsqrt += Math.Pow(A[i] - B[i], 2);
+    }
+    return Math.Sqrt(forsqrt);
+}
+
+Console.WriteLine("Введите координаты точки A (3 числа) через \"; \"");
+double[] A = GetArrayFromString(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B (3 числа) через \"; \"");
+double[] B = GetArrayFromString(Console.ReadLine());
+Console.WriteLine("Расстояние " + GetDist2Point(A, B));
