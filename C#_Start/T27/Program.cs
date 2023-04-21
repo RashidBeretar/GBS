8e6b59b852463de76a0d63944fe5b27b2c7c56e3
Rashid <rashidberetar@gmail.com>
2023-04-21 18:54:26 +0300

    add t27

---
+++ C#_Start/T27/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] GetNumberFromConsole(int cnt)
+{
+    int[] a = new int[cnt];
+    for (int i = 1; i <= cnt; i++)
+    {
+        a[i - 1] = int.Parse(Console.ReadLine());
+    }
+    return a;
+}
+int GetSumDigitFromNumber(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+        Console.WriteLine(number);
+    }
+    return result;
+}
+Console.WriteLine("Введите данные:");
+Console.WriteLine(GetSumDigitFromNumber(GetNumberFromConsole(1)[0]));
