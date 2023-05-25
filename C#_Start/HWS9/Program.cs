b95b953141a476f293ab3f4dff3e0b9a81ae7d7f
Rashid <rashidberetar@gmail.com>
2023-05-26 00:18:30 +0300

    ad HWS9

---
+++ C#_Start/HWS9/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+void OutputAllNaturalNumberLimit(int N, int X = 1)
+{
+    if (X != N) OutputAllNaturalNumberLimit(N, X + 1);
+    if (X == 1) Console.WriteLine($"{X}");
+    else Console.Write($"{X}, ");
+}
+// OutputAllNaturalNumberLimit(5);
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int GetNaturalNumberInterval(int M, int N)
+{
+    if (M < N) (M, N) = (N, M);
+    if (N != M) return N + GetNaturalNumberInterval(M, N + 1);
+    return N;
+}
+// Console.WriteLine(GetNaturalNumberInterval(4, 8));
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+int AckermanFunction(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else if (m == 0) return AckermanFunction(n - 1, 1);
+    else return AckermanFunction(n - 1, AckermanFunction(n, m - 1));
+}
+// Console.WriteLine(AckermanFunction(2, 3));
+Console.WriteLine(AckermanFunction(3, 2));
+// Console.WriteLine(AckermanFunction(3, 4));
---
+++ C#_Start/T63/Program.cs
@@ -0,0 +1,57 @@
+﻿// T63
+void OutputNaturalNumber(int N)
+{
+    if (N >= 1)
+    {
+        OutputNaturalNumber(N - 1);
+    }
+    Console.Write($"{N}, ");
+}
+
+// T65
+void OutputNaturalNumberInterval(int M, int N)
+{
+    if (N != M)
+    {
+        OutputNaturalNumberInterval(M, N - 1);
+    }
+    Console.Write($"{N}, ");
+}
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// 4789
+// 9
+// 478
+// 8
+// 47
+
+int T67(int number)
+{
+    if (number / 10 > 0)
+    {
+        return number % 10 + T67(number / 10);
+    }
+    return number % 10;
+}
+// Console.Write(T67(457));
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int T69(int number, int extent)
+{
+    if (extent > 0)
+    {
+        return number * T69(number, extent - 1);
+    }
+    return 1;
+}
+
+// OutputNaturalNumber(5);
+OutputNaturalNumberInterval(5, 7);
+// Console.Write(T67(457));
+// Console.Write(T69(5, 3));
